e46b5f78ea968747037409eac16220db7fadb8ec
tfgungun <gunawan48160@gmail.com>
2020-03-22 22:26:04 +0700

    fix gui

--- CalculatorApp/Form1.cs
+++ CalculatorApp/Form1.cs
@@ -15,9 +15,14 @@ namespace CalculatorApp
         Double value = 0;
         String operation = "";
         bool operation_pressed = false;
+        Queue<Elemen<string>> operationQueue;
+        QueueProcessor queueOp;
         public Form1()
         {
             InitializeComponent();
+            operationQueue = new Queue<Elemen<string>>();
+            queueOp = new QueueProcessor();
+            queueOp.setQueue(operationQueue);
         }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -52,32 +57,63 @@ namespace CalculatorApp
         private void operator_Click(object sender, EventArgs e)
         {
             Button b = (Button)sender;
-            bool Eq = false;
-            Queue<Elemen<string>> operationQueue = new Queue<Elemen<string>>();
-            QueueProcessor queueOp = new QueueProcessor();
-            queueOp.setQueue(operationQueue);
+            bool Eq = (b.Text == "=");
 
-            while (!Eq)
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text += " " + value + " " + operation;
+
+            Elemen<String> operand = new Elemen<String>(value.ToString());
+            Elemen<String> clicked = new Elemen<String>(operation);
+            operationQueue.Enqueue(operand);
+            if (!Eq)
             {
-                operation = b.Text;
-                value = Double.Parse(result.Text);
-                operation_pressed = true;
-                equation.Text = value + " " + operation;
-
-                Elemen<string> clicked = new Elemen<string>(b.Text);
+                Console.WriteLine("masuk");
                 operationQueue.Enqueue(clicked);
-
-                if (b.Text == "=")
-                {
-                    Eq = true;
-                }
             }
-            Console.WriteLine(queueOp.solveQueue());
+            else
+            {
+                Console.WriteLine("hasil");
+                Console.WriteLine(queueOp.solveQueue());
+            }
+
+            /* while (!Eq)
+             {
+                 Console.WriteLine("masuk");
+                 operation = b.Text;
+                 value = Double.Parse(result.Text);
+                 operation_pressed = true;
+                 equation.Text = value + " " + operation;
+
+                 Elemen<string> operand = new Elemen<String>(value.ToString());
+                 Elemen<string> clicked = new Elemen<string>(b.Text);
+                 operationQueue.Enqueue(operand);
+
+                 if (b.Text == "=")
+                 {
+                     Eq = true;
+                 }
+                 else
+                 {
+                     operationQueue.Enqueue(clicked);
+                 }
+             }*/
         }
 
         private void button18_Click(object sender, EventArgs e)
         {
-            equation.Text = "";
+            value = Double.Parse(result.Text);
+            equation.Text += " " + value + " " + operation;
+
+            Elemen<String> operand = new Elemen<String>(value.ToString());
+            operationQueue.Enqueue(operand);
+            Console.WriteLine("hasil");
+            double solveRes = queueOp.solveQueue();
+            Console.WriteLine(solveRes);
+            result.Text = (solveRes).ToString();
+
+            /*equation.Text = "";
             switch (operation)
             {
                 case "+":
@@ -94,7 +130,7 @@ namespace CalculatorApp
                     break;
                 default:
                     break;
-            }//end switch
+            }//end switch*/
         }
 
         private void button15_Click(object sender, EventArgs e)
@@ -105,6 +141,11 @@ namespace CalculatorApp
         }
 
         private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
